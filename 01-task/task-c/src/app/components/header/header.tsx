44128ff51ac8098760f90382ccb2512028933978
Daniel Dushev <danieldyshew@gmail.com>
2024-04-20 19:03:43 +0300

    header & globals. add hamburger menu transitions

--- 01-task/task-c/src/app/components/header/header.tsx
+++ 01-task/task-c/src/app/components/header/header.tsx
@@ -26,13 +26,13 @@ function Header() {
           <a className="font-semibold border-solid border-2 border-primary rounded-lg p-2 hover:bg-primary hover:text-white" href="#">Заявка за пратка</a>
         </div>
         {/* Mobile menu */}
-        <div className="lg:hidden">
-          <button onClick={() => setMobileMenuOpen(!isMobileMenuOpen)} type="button" className="transition-all w-6 h-6 relative">
-            <span className="absolute top-0 left-0 w-6 h-0.5 bg-primary transition-all rotate-0"></span>
-            <span className="absolute top-0 left-0 w-6 h-0.5 bg-primary transition-all rotate-0 translate-y-2"></span>
-            <span className="absolute top-0 left-0 w-6 h-0.5 bg-primary transition-all rotate-0 translate-y-4"></span>
+        <div className="lg:hidden pt-3">
+          <button onClick={() => setMobileMenuOpen(!isMobileMenuOpen)} type="button" className={cx("transition-all w-6 h-6 relative", { 'hamburger': isMobileMenuOpen })}>
+            <span className={cx("absolute transition-all top-0 left-0 w-6 h-0.5 bg-primary", { 'hamburger-top': isMobileMenuOpen })}></span>
+            <span className={cx("absolute transition-all top-0 left-0 w-6 h-0.5 bg-primary translate-y-[7px]", { 'hamburger-middle': isMobileMenuOpen })}></span>
+            <span className={cx("absolute transition-all top-0 left-0 w-6 h-0.5 bg-primary translate-y-[14px]", { 'hamburger-bottom': isMobileMenuOpen })}></span>
           </button>
-          <div className={cx("absolute flex-col py-8 space-y-6 left-6 right-6", { flex: isMobileMenuOpen, hidden: !isMobileMenuOpen })}>
+          <div className={cx("absolute flex-col py-8 space-y-6 left-6 right-6", { 'flex': isMobileMenuOpen, 'hidden': !isMobileMenuOpen })}>
             <a className="p-2 hover:bg-primary hover:text-white rounded text-greenish" href="#">Начало</a>
             <a className="p-2 hover:bg-primary hover:text-white rounded text-grayish" href="#">За нас</a>
             <a className="p-2 hover:bg-primary hover:text-white rounded text-grayish" href="#">Услуги</a>
@@ -42,9 +42,9 @@ function Header() {
             <a className="p-2 hover:bg-primary hover:text-white rounded text-greenish" href="#">Профил</a>
             <a className="font-semibold border-solid border-2 border-primary rounded-lg p-2 hover:bg-primary hover:text-white" href="#">Заявка за пратка</a>
           </div>
-        </div>
+        </div >
 
-      </nav>
+      </nav >
     </header >
   )
 }
