46847a7fbae5a742cead37598802bbb2c67b799e
Daniel Dushev <danieldyshew@gmail.com>
2024-04-20 22:10:45 +0300

    hero. add hero h1

---
+++ 01-task/task-c/src/components/hero/hero.tsx
@@ -0,0 +1,12 @@
+
+function Hero() {
+  return (
+    <section className="flex flex-col container mx-auto">
+      <div className="flex justify-center">
+        <h1 className="font-grotesk text-5xl tracking-tighter text-center px-5 w-[930px] md:text-6xl lg:text-7xl">Гъвкави решения за сухопътен транспорт за всеки клиент</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
