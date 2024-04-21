ec2dd9096791afcb69478a83c921ded92c7e9488
Daniel Dushev <danieldyshew@gmail.com>
2024-04-21 21:41:36 +0300

    card. add darker hover effect

--- 01-task/task-c/src/components/footer/card.tsx
+++ 01-task/task-c/src/components/footer/card.tsx
@@ -9,7 +9,7 @@ interface ICardProps {
 function Card({ imgSrc, alt, text }: ICardProps) {
   return (
     <div className="flex flex-col gap-8">
-      <a href="#">
+      <a className=" hover:bg-black rounded-2xl" href="#">
         <Image className="transition-all duration-200 hover:opacity-75 rounded-xl" src={imgSrc} width={416} height={364} alt={alt} />
       </a>
       <h4 className="font-grotesk font-semibold text-center text-xl tracking-tight lg:text-2xl lg:text-left">{text}</h4>
