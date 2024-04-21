ebc6739d923b8a14acb0fb0acc770187f237d6dc
Daniel Dushev <danieldyshew@gmail.com>
2024-04-21 20:23:33 +0300

    card. add hover effects

--- 01-task/task-c/src/components/footer/card.tsx
+++ 01-task/task-c/src/components/footer/card.tsx
@@ -8,8 +8,10 @@ interface ICardProps {
 
 function Card({ imgSrc, alt, text }: ICardProps) {
   return (
-    <div className="flex flex-col gap-8 ">
-      <Image className="rounded-xl" src={imgSrc} width={416} height={364} alt={alt} />
+    <div className="flex flex-col gap-8">
+      <a href="#">
+        <Image className="transition-all duration-200 hover:opacity-75 rounded-xl" src={imgSrc} width={416} height={364} alt={alt} />
+      </a>
       <h4 className="font-grotesk font-semibold text-center text-xl tracking-tight lg:text-2xl lg:text-left">{text}</h4>
     </div>
   )
