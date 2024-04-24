e5ba49cbfe323f7f5ccc6f1ec1a2b2f3c26bc6fb
Daniel Dushev <danieldyshew@gmail.com>
2024-04-24 13:52:59 +0300

    primaryButton. fix dynamic colors and padding

--- 02-task/src/components/ui/primaryButton.tsx
+++ 02-task/src/components/ui/primaryButton.tsx
@@ -1,13 +1,20 @@
 import cx from "classnames";
 
-function PrimaryButton({ text, paddingY = "2" }: { text: string, paddingY?: string }) {
+interface IPrimaryButton {
+  text: string,
+  paddingY?: "2" | "4",
+  bgColor?: "white" | "primary"
+}
+
+function PrimaryButton({ text, paddingY = "2", bgColor = "primary" }: IPrimaryButton) {
   const buttonClass = cx(
     "px-6",
     "tracking-[-0.2px]",
     "rounded",
-    `py-${paddingY}`,
-    "bg-white",
-    "hover:bg-primary",
+    `${paddingY === "2" ? "py-2" : "py-4"}`,
+    `${bgColor === "white" ? "text-black" : "text-white"}`,
+    `${bgColor === "white" ? "bg-white" : "bg-primary"}`,
+    "hover:bg-black",
     "hover:text-white"
   );
   return (
