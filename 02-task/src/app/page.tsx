7c6558b1c64ab095890e864632655e8c010d33fe
Daniel Dushev <danieldyshew@gmail.com>
2024-04-26 12:22:44 +0300

    services. add component header part

--- 02-task/src/app/page.tsx
+++ 02-task/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/layout/hero";
+import Services from "@/components/layout/services";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
---
+++ 02-task/src/components/layout/services.tsx
@@ -0,0 +1,17 @@
+
+function Services() {
+  return (
+    <section className="bg-primary">
+      <div className="outer-container py-20 lg:py-32">
+        <div className="inner-container text-white">
+          <div className="flex flex-col items-center gap-5 text-center lg:flex-row lg:text-left">
+            <h2 className="text-5xl font-bold pb-14 sm:max-2xl:w-[80%]">Here&apos;s how Roooby can benefit your business</h2>
+            <p className="text-lg font-normal max-w-[490px]">Build more meaningful and lasting relationships — better understand their needs, identify new opportunities to help, address any problems faster.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
