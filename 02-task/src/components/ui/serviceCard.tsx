a7b95ebdfe7cda7913a2cd6a6da97c900e15c838
Daniel Dushev <danieldyshew@gmail.com>
2024-04-27 12:43:43 +0300

    serviceCard. add hover effect changing bg and content colors

--- 02-task/src/components/ui/serviceCard.tsx
+++ 02-task/src/components/ui/serviceCard.tsx
@@ -1,4 +1,8 @@
+"use client";
+import cx from "classnames";
 import Image from "next/image"
+import { useEffect, useState } from "react";
+
 
 interface IServiceCardProps {
   src: string,
@@ -10,13 +14,14 @@ interface IServiceCardProps {
 }
 
 function ServiceCard({ src, width, height, alt, heading, text }: IServiceCardProps) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="rounded-lg w-[374px] bg-[#1E0A57]">
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className="rounded-lg w-[374px] bg-[#1E0A57] hover:bg-yellow-400 hover:text-black">
       <div className="w-[90%] mx-auto mb-20">
         <div className="size-20 mt-16 mb-14">
-          <Image className="" src={src} width={width} height={height} alt={alt} />
+          <Image className={isHovered ? "invert" : ""} src={src} width={width} height={height} alt={alt} style={{ width: "auto", height: "auto" }} />
         </div>
-        <div className="h-[1px] bg-[#54457F]"></div>
+        <div className={cx("h-[1px] bg-[#54457F]", { "bg-black": isHovered })}></div>
         <h4 className="text-xl font-bold tracking-tighter-[0.38px] py-4 max-w-[284px]">
           {heading}
         </h4>
@@ -25,7 +30,7 @@ function ServiceCard({ src, width, height, alt, heading, text }: IServiceCardPro
         </p>
       </div>
 
-    </div>
+    </div >
   )
 }
 
